86f78ab1006040f5edc2a486c70e1eb6eeefe667
Khayat Bertrand (External) <Bertrand.Khayat.Guest@saipem.com>
2025-03-30 23:32:43 +0200

    remove filter

--- example/main_degiro.py
+++ example/main_degiro.py
@@ -46,7 +46,7 @@ if __name__ == "__main__":
 
     rdcf_anal = RDCFAnal(config_dict)
 
-    rdcf_anal.share_list = [ s for s in rdcf_anal.share_list if s.symbol in [ "INFY"] ]
+    # rdcf_anal.share_list = [ s for s in rdcf_anal.share_list if s.symbol in [ "INFY"] ]
     
     # rdcf_anal.load_df()
     rdcf_anal.process()
